4963b7fc99e81ef80034be42c51124de3b7596db
andcappe <andcappe@gmail.com>
2026-04-28 22:10:38 +0200

    aggiungi Frontiera Efficiente su /frontiera-efficiente/

--- wsgi.py
+++ wsgi.py
@@ -1,1 +1,7 @@
-from app import server as application
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from app import server as main_server
+from frontiera_app import frontier_server
+
+application = DispatcherMiddleware(main_server, {
+    '/frontiera-efficiente': frontier_server,
+})
